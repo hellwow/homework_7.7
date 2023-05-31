75de2fc9203848df2c3d43298caeb518495636fc
hellwow <timofei.korchaka@yandex.ru>
2023-06-01 02:19:33 +0300

    fixed files script and css

--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -17,13 +17,26 @@ document.getElementById('btnRetry').addEventListener('click', function () {
     orderNumber = 0;
 })
 
+document.getElementById('btnRetry').addEventListener('click', function () {
+    if(gameRun) {
+        minValue = 0;
+        maxValue = 100;
+        orderNumber = 0;
+        gameRun = true;
+    } else {
+        alert("Попытки закончились, попробуем снова?");
+        location.reload();
+    }    
+})
+ 
 document.getElementById('btnOver').addEventListener('click', function () {
     if (gameRun){
         if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
+            const phraseRandom = Math.round( Math.random()*3);
             const answerPhrase = (phraseRandom === 1) ?
                 `Вы загадали неправильное число!\n\u{1F914}` :
                 `Я сдаюсь..\n\u{1F92F}`;
+                `Давайте еще в степень возведем!\n\u{1F939}`;
 
             answerField.innerText = answerPhrase;
             gameRun = false;
@@ -37,10 +50,30 @@ document.getElementById('btnOver').addEventListener('click', function () {
     }
 })
 
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun){
+        if (minValue === maxValue){
+            const phraseRandom = Math.round( Math.random()*3);
+            const answerPhrase = (phraseRandom === 1) ?
+                `Пожалуйста, перестаньте тыкать на кнопки!\n\u{1F939}` :
+                `Боюсь это слишком сложно для меня..\n\u{1F91E}` ;
+                `Так может продолжаться до бесконечности..\n\u{1F93F}` ;
+
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber  + 1;
+            answerNumber  = Math.floor((maxValue + minValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+        }
+    }
+})
+
 document.getElementById('btnEqual').addEventListener('click', function () {
     if (gameRun){
         answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
         gameRun = false;
     }
 })
-
